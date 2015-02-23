7476cd1f3b8c9bea7155fbbe29022629a09db3bb
Ivan Mikushin <i.mikushin@gmail.com>
2015-02-23 18:48:02 +0500

    rm some noise

--- test/hello/core_test.cljs
+++ test/hello/core_test.cljs
@@ -1,14 +1,6 @@
 (ns hello.core-test
-  (:require [expectations :refer [in localize no-op] :refer-macros [expect
-                                                                    expect-focused
-                                                                    expect-let
-                                                                    expect-let-focused
-                                                                    from-each
-                                                                    more
-                                                                    more->
-                                                                    more-of
-                                                                    redef-state
-                                                                    side-effects]]))
+  (:require [expectations :refer-macros [expect
+                                         expect-focused]]))
 
 (expect 1 1)
 
